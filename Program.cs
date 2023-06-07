0692243e56d8f960d4e8790f8674a33c5edcc816
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-06-06 19:37:09 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,79 @@
-﻿namespace Prog_120_S23_L17_LinearSearch
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace Prog_120_S23_L17_LinearSearch
 {
     internal class Program
     {
         static int[] numbers = { 10, 50, 30, 70, 80, 20, 90, 40 };
+        static int[] random = { 37, 81, 5, 47, 19, 92, 76, 52, 63, 88, 10, 24, 42, 60, 98, 15, 72, 34, 50, 3 };
+
+        static string[] names = { "Benel", "David", "Will", "Josh" };
+        static string[] className = { "Prog 120", "Prog 120", "Prog 124", "CSI 234" };
+        static string[] roomNumbers = { "Online", "102", "111", "106" };
+
+
+
+
+
 
         static void Main(string[] args)
         {
-            // Linear Search
-            
+
+            string search = "Prog 124";
+            int locationOfSearch = LocationOf(search, className);
+
+            Console.WriteLine($"{names[locationOfSearch]} - Program : {className[locationOfSearch]} - Location : {roomNumbers[locationOfSearch]}");
+
+
+
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine($"{names[i]} - Program : {className[i]} - Location : {roomNumbers[i]}");
+            //}
+
+
+
+
 
         } // Main
+
+        
+
+        // Linear search that returns true or false
+        public static bool Contains(int search, int[] array)
+        {
+            // Looping through the array
+            foreach(int number in array)
+            {
+                Console.WriteLine(number);
+                // Checking if our number exists
+                if (search == number)
+                {
+                    // Return TRUE if found
+                    return true;
+                }
+                
+            }
+            // If not found, return false
+            return false;
+        }
+
+        // What is the location of our search
+        //   0    1  2    3  4    5  6   7
+        // { 10, 50, 30, 70, 80, 20, 90, 40 };
+        public static int LocationOf(string search, string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (search == array[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
 
         public static void Example()
         {
